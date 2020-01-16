b91ab3c4ffdf36cc3b9b5f86f3d8632d29f04f06
jenniferunguyen <jenguyen@chapman.edu>
2020-01-16 19:03:19 +0000

    tested version of hello world

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+using namespace std;
 
 int main(int argc, char **argv){
-    cout >> "hello world from cpsc 298" >> endl;
+    cout << "hello world from cpsc 298" << endl;
     return 0;
 }
